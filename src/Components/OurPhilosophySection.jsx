12aeea4e75f0b9d543ff8f6bc6075a53955d74c9
Vishal Borse <borsev662@gmail.com>
2026-03-01 23:32:20 +0530

    landing section images added issue fixed

--- src/Components/OurPhilosophySection.jsx
+++ src/Components/OurPhilosophySection.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { FaChartPie } from "react-icons/fa";
+
 import { ChevronLeft, ChevronRight } from "lucide-react";
-//import blog_bg from "../images/blog-bg.png";
-// import philosopy_bg from "../images/philosopy-bg.png";
-// import philosophyBg from "../../public/images/philosopy-bg.svg";
+
 import philosophyBg from "../images/philosopy.png";
 import { TbUserStar } from "react-icons/tb";
 import { PiChartLineUpBold } from "react-icons/pi";
